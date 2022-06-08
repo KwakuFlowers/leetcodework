69b476b9dbcde83ab90bc57f86fc143742295108
KwakuFlowers <kwakuflowers@gmail.com>
2022-06-08 14:20:50 -0400

    New test for TwoSum

--- TwoSum.py
+++ TwoSum.py
@@ -1,4 +1,10 @@
 class Solution(object):
+    def findvalue(self, given, num, currind):
+        for i in enumerate(num):
+            if i[0] != currind:
+                if i[1] == given:
+                    return i[0]
+
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
@@ -16,31 +22,26 @@ class Solution(object):
         if self.currlen >= 2 and self.currlen <= (10**4):
             self.maxind = self.currlen - 1
             for e in enumerate(self.nums):
-                # print(e[0], e[1])
-                for v in enumerate(self.nums):
-                    # print(v[0], v[1])
-                    if e[0] == self.maxind and v[0] == self.maxind and self.currlen > 2:
-                        statement = "No matches found!"
-                        return statement
+                self.numlook = abs(target - e[1])
+                # print(self.numlook)
+                foundvalind = self.findvalue(self.numlook, self.nums, e[0])
+                if isinstance(foundvalind, type(None)):
+                    # print(foundvalind)
+                    if e[0] == self.maxind:
                         break
-                    if e[0] == v[0]:
-                        continue
-                    currval = self.nums[e[0]] + self.nums[v[0]]
-                    # print(currval)
-                    if currval == self.target:
-                        self.ret = [e[0], v[0]]
-                        return self.ret
-                        break
-                    else:
-                        continue
+                    continue
+                else:
+                    self.ret = [e[0], foundvalind]
                     break
+
+            return self.ret
 
 
 S = Solution()
 test = [3, 2, 4]
 
 searchtest = 6
-S.twoSum(test, searchtest)
+print(S.twoSum(test, searchtest))
 
 # [3, 15, 3, 7]
 # 6
